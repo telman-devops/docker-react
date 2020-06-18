f9b1546b7498bcadad482b88cdec0373de09c246
Iliyev Telman <iliev_kz@mail.ru>
2020-06-18 13:32:40 +0600

    Feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi From 001
+          Hi From Feature
         </p>
         <a
           className="App-link"
